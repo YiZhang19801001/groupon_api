05b735e3df745468ae234dc1a1d457a72074a721
yizhang19801001 <yizhang19801001@gmail.com>
2019-01-21 16:14:12 +1100

    cors solve

--- routes/api.php
+++ routes/api.php
@@ -17,17 +17,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('products/{language_id}', 'ProductController@index');
-Route::get('products/{language_id}/{product_id}', 'ProductController@show');
-Route::post('products', 'ProductController@create');
-Route::put('products/{product_id}', 'ProductController@update');
+Route::group(['middleware' => 'api-header'], function () {
 
-Route::get('categories/{language_id}', 'CategoryController@index');
-Route::get('categories/{language_id}/{category_id}', 'CategoryController@show');
-Route::post('categories', 'CategoryController@create');
-Route::put('categories/{category_id}', 'CategoryController@update');
+    Route::get('products/{language_id}', 'ProductController@index');
+    Route::get('products/{language_id}/{product_id}', 'ProductController@show');
+    Route::post('products', 'ProductController@create');
+    Route::put('products/{product_id}', 'ProductController@update');
 
-Route::get('locations', 'LocationController@index');
-Route::get('locations/{location_id}', 'LocationController@show');
-Route::post('locations', 'LocationController@create');
-Route::put('locations/{location_id}', 'LocationController@update');
+    Route::get('categories/{language_id}', 'CategoryController@index');
+    Route::get('categories/{language_id}/{category_id}', 'CategoryController@show');
+    Route::post('categories', 'CategoryController@create');
+    Route::put('categories/{category_id}', 'CategoryController@update');
+
+    Route::get('locations', 'LocationController@index');
+    Route::get('locations/{location_id}', 'LocationController@show');
+    Route::post('locations', 'LocationController@create');
+    Route::put('locations/{location_id}', 'LocationController@update');
+
+    // The registration and login requests doesn't come with tokens
+    // as users at that point have not been authenticated yet
+    // Therefore the jwtMiddleware will be exclusive of them
+    Route::post('user/login', 'UserController@login');
+    Route::post('user/register', 'UserController@register');
+});
